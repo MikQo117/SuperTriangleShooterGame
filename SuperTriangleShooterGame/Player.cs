18286a69e4f94d98260680ffdfcfd81f12134c5c
Peltola Mikko TTK17SP <mikkopeltola@kamk.fi>
2018-01-23 12:08:49 +0200

    Initial commit with gitignore and monogame project

---
+++ SuperTriangleShooterGame/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperTriangleShooterGame
+{
+    class Player : Character
+    {
+        public void Initialize()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
